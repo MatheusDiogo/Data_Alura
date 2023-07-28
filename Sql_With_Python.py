04546220e58e30f913a817c38b66c30da9236bf6
Matheus Diogo <matheusdiogoeng@gmail.com>
2023-07-27 23:12:42 -0300

    Lendo dados das planilhas

--- Sql_With_Python.py
+++ Sql_With_Python.py
@@ -1,2 +1,13 @@
 import os
 import pandas as pd
+
+url_itens_pedidos = 'https://github.com/alura-cursos/SQL-python-integracao/raw/main/TABELAS/itens_pedidos.csv'
+url_pedidos = 'https://github.com/alura-cursos/SQL-python-integracao/raw/main/TABELAS/pedidos.csv'
+url_produto = 'https://github.com/alura-cursos/SQL-python-integracao/raw/main/TABELAS/produtos.csv'
+url_vendedores = 'https://github.com/alura-cursos/SQL-python-integracao/raw/main/TABELAS/vendedores.csv'
+
+itens_pedidos = pd.read_csv(url_itens_pedidos)
+pedidos = pd.read_csv(url_pedidos)
+produtos = pd.read_csv(url_produto)
+vendedores = pd.read_csv(url_vendedores)
+
