7d010b67b0e194d1419468465780695d3d0052a1
Matheus Diogo <matheusdiogoeng@gmail.com>
2023-07-27 23:30:06 -0300

    Iniciando banco de dados

--- Sql_With_Python.py
+++ Sql_With_Python.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+from sqlalchemy import create_engine, inspect, text
 
 url_itens_pedidos = 'https://github.com/alura-cursos/SQL-python-integracao/raw/main/TABELAS/itens_pedidos.csv'
 url_pedidos = 'https://github.com/alura-cursos/SQL-python-integracao/raw/main/TABELAS/pedidos.csv'
@@ -11,3 +12,12 @@ pedidos = pd.read_csv(url_pedidos)
 produtos = pd.read_csv(url_produto)
 vendedores = pd.read_csv(url_vendedores)
 
+engine = create_engine('sqlite:///:memory:')
+
+produtos.to_sql('produtos',engine,index=False)
+itens_pedidos.to_sql('itens_pedidos', engine, index=False)
+pedidos.to_sql('pedidos', engine, index=False)
+vendedores.to_sql('vendedores', engine, index=False)
+
+inspector = inspect(engine)
+print(inspector.get_table_names())
